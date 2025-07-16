0cf00d434806f45ccde596e145ef74bc52915497
Mister-deal <max-94400@hotmail.fr>
2025-07-16 14:15:21 +0200

    ajout classe commandes et ajout enum type

--- Classes/Commandes.cs
+++ Classes/Commandes.cs
@@ -2,5 +2,9 @@
 {
     public class Commandes
     {
+        private int id_commandes;
+        private DateTime date_commandes;
+        private TypeCommande type_commandes;
+        private bool est_termine;
     }
 }
---
+++ Classes/TypeCommande.cs
@@ -0,0 +1,8 @@
+﻿namespace RestauSimplon.Classes;
+
+public enum TypeCommande
+{
+    Livraison,
+    Sur_Place,
+    A_Emporter
+}
