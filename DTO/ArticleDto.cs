7c43069a27af6f111abe40e39d21bd726c7fd937
SymBiios <julien.sarkarin@gmail.com>
2025-07-17 10:49:05 +0200

    feat: ajout des dto pour article et client

---
+++ DTO/ArticleDto.cs
@@ -0,0 +1,17 @@
+﻿using RestauSimplon.Classes;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestauSimplon.DTO;
+
+public class ArticleDto
+{
+    public int Id { get; set; }
+    public string Nom { get; set; }
+    public decimal Prix { get; set; }
+    public Categorie Categorie { get; set; }
+    public string? Description { get; set; }
+    public bool Disponible { get; set; }
+
+    // Propriété pour afficher le nom de la catégorie
+    public string NomCategorie => Categorie.ToString();
+}
