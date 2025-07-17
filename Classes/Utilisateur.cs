d6d564d4b32ca34e369b0a66a8a0dc161f1cf7d2
Cédric <134500848+Muryokukan@users.noreply.github.com>
2025-07-17 15:54:33 +0200

    refacto: added "required"

--- Classes/Utilisateur.cs
+++ Classes/Utilisateur.cs
@@ -3,8 +3,8 @@ namespace RestauSimplon.Classes
     public class Utilisateur
     {
         public int Id { get; set; }
-        public string NomDeCompte { get; set; }
-        public string MdpHash { get; set; }
+        public required string NomDeCompte { get; set; }
+        public required string MdpHash { get; set; }
         public bool EstAdmin { get; set; }
     }
 }
