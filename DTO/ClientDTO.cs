bc3391a9fa5bc45d2ec3dfcb82e4bf20dbe8f387
GoutteDooo <goudal.donovan@live.fr>
2025-07-17 12:30:02 +0200

    chore(Routes/Clients): Ajout de la méthode GET pour obtenir un client par son ID

--- DTO/ClientDTO.cs
+++ DTO/ClientDTO.cs
@@ -1,19 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using RestauSimplon.Classes;
 namespace RestauSimplon.DTO;
-
-
 public class ClientDto
 {
     public int Id { get; set; }
     public string Nom { get; set; }
     public string Prenom { get; set; }
-    public string Numero_rue { get; set; }
-    public string Nom_rue { get; set; }
+    public string NumeroRue { get; set; }
+    public string NomRue { get; set; }
     public string Ville { get; set; }
-    public int Code_postal { get; set; }
-    public int Telephone { get; set; }
+    public int CodePostal { get; set; }
+    public string Telephone { get; set; }
+
+    public ClientDto() { }
+    public ClientDto(Client clientItem) =>
+        (Id, Nom, Prenom, NumeroRue, NomRue, Ville, CodePostal, Telephone) 
+        = (clientItem.Id, clientItem.Nom, clientItem.Prenom, clientItem.NumeroRue, clientItem.NomRue, clientItem.Ville, clientItem.CodePostal, clientItem.Telephone);
 
     // Propriété calculée pour l'adresse complète
-    public string AdresseComplete => $"{Numero_rue} {Nom_rue}, {Code_postal} {Ville}";
+    public string AdresseComplete => $"{NumeroRue} {NomRue}, {CodePostal} {Ville}";
 }
