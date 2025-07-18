1127d4838ceb2a2ee6ae795aa97df42b1358da6c
Cédric L. <c.limosin@outlook.fr>
2025-07-18 12:14:50 +0200

    feat: installed Swagger

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using RestauSimplon.Classes;
 using RestauSimplon.Data;
 using RestauSimplon.Routes;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace RestauSimplon
 {
@@ -12,7 +14,38 @@ namespace RestauSimplon
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<RestaurantDb>(opt => opt.UseSqlite("Data source=resto.db"));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API Restau Simplon",
+                    Version = "v1",
+                    Description = "Miam miam",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Toto",
+                        Email = "Supertoto@exemple.ex",
+                        Url = new Uri("https://restau-simplon.com"),
+                    }
+                });
+            });
+
             var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            { 
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Restau API V1");
+                    c.RoutePrefix = "";
+                });
+            
+            
+            }
+
 
             /* ROUTES */
 
